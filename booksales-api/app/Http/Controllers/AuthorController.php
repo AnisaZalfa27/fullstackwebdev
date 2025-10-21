f61d5585887b3da3d511ee7812a01f76bbb43455
Zalfa1231 <khoirunnisazalfa005@gmail.com>
2025-10-21 17:20:31 +0700

    controller

--- booksales-api/app/Http/Controllers/AuthorController.php
+++ booksales-api/app/Http/Controllers/AuthorController.php
@@ -3,12 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Models\Author;
+use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    public function index()
+    // Menampilkan data author berdasarkan ID
+    public function show($id)
     {
-        $authors = Author::all();
-        return view('authors.index', compact('authors'));
+        $author = Author::find($id);
+        if (!$author) {
+            return response()->json(['message' => 'Author not found'], 404);
+        }
+        return response()->json($author);
+    }
+
+    // Memperbarui data author berdasarkan ID
+    public function update(Request $request, $id)
+    {
+        $author = Author::find($id);
+        if (!$author) {
+            return response()->json(['message' => 'Author not found'], 404);
+        }
+
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:authors,email,' . $id,
+        ]);
+
+        $author->update($validated);
+        return response()->json($author);
+    }
+
+    // Menghapus data author berdasarkan ID
+    public function destroy($id)
+    {
+        $author = Author::find($id);
+        if (!$author) {
+            return response()->json(['message' => 'Author not found'], 404);
+        }
+
+        $author->delete();
+        return response()->json(['message' => 'Author deleted successfully']);
     }
 }
