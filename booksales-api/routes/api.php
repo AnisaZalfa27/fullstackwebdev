0bd4d6db7662d2746e172ddf359e2f26383df558
Zalfa1231 <khoirunnisazalfa005@gmail.com>
2025-10-21 17:31:58 +0700

    routing

---
+++ booksales-api/routes/api.php
@@ -0,0 +1,22 @@
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\GenreController;
+
+
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{id}', [AuthorController::class, 'show']);
+Route::get('/genres', [GenreController::class, 'index']);
+Route::get('/genres/{id}', [GenreController::class, 'show']);
+
+
+Route::middleware(['auth:api', 'role:admin'])->group(function () {
+    // Author
+    Route::post('/authors', [AuthorController::class, 'store']);
+    Route::put('/authors/{id}', [AuthorController::class, 'update']);
+    Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
+
+    // Genre
+    Route::post('/genres', [GenreController::class, 'store']);
+    Route::put('/genres/{id}', [GenreController::class, 'update']);
+    Route::delete('/genres/{id}', [GenreController::class, 'destroy']);
+});
